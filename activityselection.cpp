92b184437cb0372b33a3e127910b183059c016fa
Supreet Kaur <badwalsupreet2439@gmail.com>
2025-04-09 11:41:39 +0530

    Update activityselection.cpp

--- activityselection.cpp
+++ activityselection.cpp
@@ -1,55 +1,64 @@
-
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-struct activity{
-
+struct activity {
     int start, end;
 };
 
-
-void solve(int n, vector<activity> actvty){
-
+void solve(int n, vector<activity> actvty) {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    int x, y;
-    for (auto x: actvty){
-    
-        // Since we sort by end time so we will insert
-        //activities in priority queue with a finish and start time
-        pq.push({x.end, x.start});
+
+    for (auto act : actvty) {
+        pq.push({act.end, act.start});  // (end, start)
     }
 
-    cout << "The activities are selected in following order: ";
-    
-    // We will always select the activity with least finish time
-    x = 0;
+    cout << "\nThe activities selected in the following order:\n";
+
+    if (pq.empty()) {
+        cout << "No activities provided.\n";
+        return;
+    }
 
     auto current = pq.top();
-    auto start = current.second;
-    auto end = current.first;
-
+    int start = current.second;
+    int end = current.first;
     pq.pop();
 
     cout << "{" << start << "," << end << "} ";
 
-    while (!pq.empty()){
-    
+    while (!pq.empty()) {
         current = pq.top();
         pq.pop();
 
-      
-        if (current.second >= end){
-        
+        if (current.second >= end) {
             start = current.second;
             end = current.first;
-
             cout << "{" << start << "," << end << "} ";
         }
     }
-}
-int main(){
 
-    int n = 6;
-    vector<activity> actvty = {{1, 2}, {4, 4}, {5, 10}, {9, 10}, {7, 8}, {2, 4}};
-    solve(n, actvty);
+    cout << endl;
 }
+
+int main() {
+    int n;
+    cout << "Enter number of activities: ";
+    cin >> n;
+
+    vector<activity> actvty(n);
+
+    cout << "Enter start and end time for each activity:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Activity " << i + 1 << " - Start: ";
+        cin >> actvty[i].start;
+        cout << "Activity " << i + 1 << " - End: ";
+        cin >> actvty[i].end;
+    }
+
+    solve(n, actvty);
+
+    return 0;
+}
+
