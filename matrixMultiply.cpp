0beb61c242d008a8372189de51be02c109dd82b3
Supreet Kaur <badwalsupreet2439@gmail.com>
2025-04-23 18:10:18 +0530

    Update matrixMultiply.cpp

--- matrixMultiply.cpp
+++ matrixMultiply.cpp
@@ -1,48 +1,72 @@
-#include<iostream>
+#include <iostream>
+#include <vector>2
 using namespace std;
+
+using Matrix = vector<vector<int>>;
+
+Matrix add(const Matrix &A, const Matrix &B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+    return C;
+}
+
+Matrix multiply(const Matrix &A, const Matrix &B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    if (n == 1) {
+        C[0][0] = A[0][0] * B[0][0];
+        return C;
+    }
+    int k = n / 2;
+    Matrix A11(k, vector<int>(k)), A12(k, vector<int>(k)), A21(k, vector<int>(k)), A22(k, vector<int>(k));
+    Matrix B11(k, vector<int>(k)), B12(k, vector<int>(k)), B21(k, vector<int>(k)), B22(k, vector<int>(k));
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < k; j++) {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][j + k];
+            A21[i][j] = A[i + k][j];
+            A22[i][j] = A[i + k][j + k];
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][j + k];
+            B21[i][j] = B[i + k][j];
+            B22[i][j] = B[i + k][j + k];
+        }
+    Matrix C11 = add(multiply(A11, B11), multiply(A12, B21));
+    Matrix C12 = add(multiply(A11, B12), multiply(A12, B22));
+    Matrix C21 = add(multiply(A21, B11), multiply(A22, B21));
+    Matrix C22 = add(multiply(A21, B12), multiply(A22, B22));
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < k; j++) {
+            C[i][j] = C11[i][j];
+            C[i][j + k] = C12[i][j];
+            C[i + k][j] = C21[i][j];
+            C[i + k][j + k] = C22[i][j];
+        }
+    return C;
+}
+
 int main() {
-   int z[2][2];
-   int i, j;
-   int m1, m2, m3, m4 , m5, m6, m7;
-      int x[2][2] = {
-         {12, 34}, 
-         {22, 10}
-      };
-   int y[2][2] = {
-      {3, 4}, 
-      {2, 1}
-   };
-   cout<<"The first matrix is: ";
-   for(i = 0; i < 2; i++) {
-      cout<<endl;
-      for(j = 0; j < 2; j++)
-         cout<<x[i][j]<<" ";
-   }
-   cout<<"\nThe second matrix is: ";
-   for(i = 0;i < 2; i++){
-      cout<<endl;
-      for(j = 0;j < 2; j++)
-         cout<<y[i][j]<<" ";
-   }
-
-   m1 = (x[0][0] + x[1][1]) * (y[0][0] + y[1][1]);
-   m2 = (x[1][0] + x[1][1]) * y[0][0];
-   m3 = x[0][0] * (y[0][1] - y[1][1]);
-   m4 = x[1][1] * (y[1][0] - y[0][0]);
-   m5 = (x[0][0] + x[0][1]) * y[1][1];
-   m6 = (x[1][0] - x[0][0]) * (y[0][0]+y[0][1]);
-   m7 = (x[0][1] - x[1][1]) * (y[1][0]+y[1][1]);
-
-   z[0][0] = m1 + m4- m5 + m7;
-   z[0][1] = m3 + m5;
-   z[1][0] = m2 + m4;
-   z[1][1] = m1 - m2 + m3 + m6;
-
-   cout<<"\nProduct achieved using Strassen's algorithm: ";
-   for(i = 0; i < 2 ; i++) {
-      cout<<endl;
-      for(j = 0; j < 2; j++)
-         cout<<z[i][j]<<" ";
-   }
-   return 0;
+    int n;
+    cout << "Enter matrix size (power of 2): ";
+    cin >> n;
+    Matrix A(n, vector<int>(n)), B(n, vector<int>(n));
+    cout << "Enter elements of Matrix A:" << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> A[i][j];
+    cout << "Enter elements of Matrix B:" << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> B[i][j];
+    Matrix C = multiply(A, B);
+    cout << "Product matrix is:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << C[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
 }
